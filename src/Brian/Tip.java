79e19d4805d0e103329053cca38596969ca24335
BKoro6 <153461678+BKoro6@users.noreply.github.com>
2025-11-30 19:21:46 +0000

    Tip.java

--- src/Brian/Tip.java
+++ src/Brian/Tip.java
@@ -9,5 +9,36 @@ package Brian;
  * @author apple
  */
 public class Tip {
-    
+
+    private String description;
+    private boolean adopted;
+
+    public Tip() {
+    }
+
+    public Tip(String description, boolean adopted) {
+        this.description = description;
+        this.adopted = adopted;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAdopted() {
+        return adopted;
+    }
+
+    public void setAdopted(boolean adopted) {
+        this.adopted = adopted;
+    }
+
+    @Override
+    public String toString() {
+        return description + " | Adopted: " + adopted;
+    }
 }
