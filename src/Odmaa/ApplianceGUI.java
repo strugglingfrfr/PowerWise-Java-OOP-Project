cf1931ca4361a73dfcbc2950ae459af7f33c5fe7
Odmaa Davaa <odmaa1008@gmail.com>
2025-11-30 13:48:35 +0000

    update

--- src/Odmaa/ApplianceGUI.java
+++ src/Odmaa/ApplianceGUI.java
@@ -96,7 +96,7 @@ public class ApplianceGUI extends javax.swing.JFrame {
 
         backgroundLBL.setIcon(new javax.swing.ImageIcon(getClass().getResource("/power/wise/app/icons/appliancebackground.jpg"))); // NOI18N
         backgroundLBL.setText("jLabel1");
-        mainPanel.add(backgroundLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(-5, -2, 930, 430));
+        mainPanel.add(backgroundLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(-5, -2, 930, 530));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
